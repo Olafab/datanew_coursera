75884c944aeb7d6b18d4d6059dc5d7a20e44ec2f
Olafab <65599998+Olafab@users.noreply.github.com>
2021-10-02 10:27:56 +0100

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,7 @@
+animals = ['cat','lion','tiger','puma']
+for types in animals:
+	print(types.title())
+	print(f"A {types.title()} would make a great pet.\n")
+
+
+print("They're really cool and slick animals.")
